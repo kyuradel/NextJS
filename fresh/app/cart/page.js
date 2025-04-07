be80e06f6fb5d4fdd867622715294730ffd604f3
kyuradel <kyuradel@gmail.com>
2025-04-07 22:54:19 +0900

    '2025-04-07'

--- fresh/app/cart/page.js
+++ fresh/app/cart/page.js
@@ -1,8 +1,32 @@
-export default function CART() {
-    
-    return (
-      <div>
-        <h1 className="title">장바구니입니다.</h1>
+import {age, name} from './data.js'
+import hello from './hello.js'
+
+export default function Cart() {
+  return (
+    <div>
+      <h4 className="title">Cart</h4>
+      <CartItem/>
+      <CartItem/>
+      <CartItem/>
+      <Button color="red"/>
+      <Button color="blue"/>
+    </div>
+  )
+} 
+
+function CartItem() {
+  return (
+      <div className="cart-item">
+        <p>{hello}</p>
+        <p>상품명 {age}</p>
+        <p>$40</p>
+        <p>1개</p>
       </div>
-    )
-  }
+  )
+}
+
+function Button() {
+  return (
+    <button style={{ background : 'props.color' }}>Button</button>
+  )
+}
--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,14 +1,29 @@
+'use client';
+import { useState } from "react"
+
 export default function List() {
-    
+  let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+  let [수량, 수량변경] = useState([0, 0, 0])
+  
     return (
       <div>
         <h4 className="title">상품목록</h4>
-        <div className="food">
-            <h4>상품1 $40</h4>
-        </div>
-        <div className="food">
-            <h4>상품2 $40</h4>
-        </div>
+
+        
+
+        {
+          상품.map((a, i) => {
+            return (
+              <div className="food" key={i}>
+                <img src={`/food${i}.png`} className="food-img"/>
+                <h4>{a} $40</h4>
+                <button onClick={()=> { if (수량 != 0) 수량변경(수량-1) }}>-</button>
+                <span> {수량} </span>
+                <button onClick={()=> { 수량변경(수량+1) }}>+</button>
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
